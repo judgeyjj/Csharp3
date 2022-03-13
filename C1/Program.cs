2571e4841640cb57a4b8a17f4aa8949ea138c53d
judgeyjj <2179067329@qq.com>
2022-03-13 19:43:04 +0800

    Signed-off-by: judgeyjj <2179067329@qq.com>

---
+++ C1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace C1
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class List<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public Node<T> Head { get; set; }
+        public List()
+        {
+            head = tail = null;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void A1(Action<T> action)
+        {
+            Node<T> t = head;
+            while (t != null)
+            {
+                action(t.Data);
+                t = t.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 4, 8, 5, 6, 3 };
+            List<int> MyList = new List<int>();
+            for (int i = 0; i < 5; i++)
+            {
+                MyList.Add(a[i]);
+            }
+            Action<int> action = delegate (int item)
+            {
+                Console.Write(item + " ");
+            };
+            MyList.A1(action);
+            Console.WriteLine();//换行
+            int sum = 0, max = int.MinValue, min = int.MaxValue;
+            MyList.A1(x => sum += x);//求和
+            MyList.A1(x => { if (x > max) max = x; });//求最大值
+            MyList.A1(x => { if (x < min) min = x; });//求最小值
+            Console.WriteLine("最大值为 {0}\n最小值为 {1}\n和为 {2}\n", max, min, sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading;
+namespace C2
+{
+    class AlarmClock
+    {
+        private static long second = 0;
+        private static long minute = 0;
+        private static long hour = 0;
+        private static long asecond = 0;
+        private static long aminute = 0;
+        private static long ahour = 0;
+        public event Action<Object> Tick = delegate
+        {
+            ++second;
+            if (second >= 60)
+            {
+                minute += 1;
+                second = 0;
+            }
+            if (minute >= 60)
+            {
+                hour += 1;
+                minute = 0;
+            }
+            if (hour >= 24)
+                hour = 0;
+            Console.WriteLine("嘀嗒嘀嗒");
+            Console.WriteLine($"Time: {hour.ToString("00")} : {minute.ToString("00")} : {second.ToString("00")}");
+        };
+        public event Action<Object> Alarm =
+            delegate (Object obj)
+            {
+                if (ahour == hour && aminute == minute && asecond == second)
+                {
+                    Console.WriteLine("闹钟响了");
+                    Console.WriteLine("闹钟响了");
+                    Console.WriteLine("闹钟响了");
+                }
+            };
+        public void Run()
+        {
+            while (true)
+            {
+                Tick(this);
+                Alarm(this);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+        public void Run(long newh, long newm, long news)
+        {
+            hour = newh;
+            minute = newm;
+            second = news;
+        }
+
+        public void SetAlarm(long h, long m, long s)
+        {
+            ahour = h;
+            aminute = m;
+            asecond = s;
+        }
+    }
+    class Form
+    {
+        public AlarmClock clk = new AlarmClock();
+
+        public void SetTime(long h, long m, long s)
+        {
+            clk.Run(h, m, s);
+        }
+
+        public void SetAlarm(long h, long m, long s)
+        {
+            clk.SetAlarm(h, m, s);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form f1 = new Form();
+            f1.SetTime(00, 0, 0);
+            f1.SetAlarm(00, 0, 10);
+            f1.clk.Run();
+        }
+    }
+}
